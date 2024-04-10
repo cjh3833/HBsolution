867b7fadd2ab7936a142669ccef04bdf72eb557d
cjh3833 <39274352+cjh3833@users.noreply.github.com>
2024-04-10 19:01:57 +0900

    히스토그램 계산함수, 드로잉함수 -> 히스토그램 GRAY 구현 완료

--- HBsolutionEx.cpp
+++ HBsolutionEx.cpp
@@ -1,4 +1,9 @@
-/* multi pattern matching 4월8일*/
+/* 
+*  HB솔루션
+*  multi pattern matching 4월8일
+*  histogram 색 출력 구현 4월 9일
+*  히스토그램 GRAY 구현 4월 10일
+*/
 
 // pylon API를 사용하기 위한 헤더 파일 포함.
 #include <pylon/PylonIncludes.h>
@@ -23,29 +28,35 @@ using namespace std;
 // 캡처할 이미지의 수를 정의.
 static const uint32_t c_countOfImagesToGrab = 300;
 
-/* 히스토그램
-void drawHist(int histogram[])
+//히스토그램 계산 함수
+void calc_Histo(const Mat& image, Mat& hist, int bins, int range_max = 256)
 {
-    int hist_w = 659;
-    int hist_h = 494;
-    int bin_w = cvRound((double)hist_w / 256);
+    int histSize[] = { bins }; // 히스토그램 계급 개수
+    float range[] = { 0, (float)range_max }; // 0번 채널 화소값 범위
+    int channels[] = { 0 }; //채널 목록 - 단일 채널
+    const float* ranges[] = { range }; //모든 채널 화소 범위
 
-    Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(255, 255, 255));
-
-    int max = histogram[0];
-    for (int i = 1; i < 256; i++) {
-        histogram[i] = floor(((double)histogram[i] / max) * histImage.rows);
-    }
-
-    for(int i = 0; i < 255; i++) {
-        line(histImage, Point(bin_w * (i), hist_h),
-            Point(bin_w * (i), hist_h - histogram[i]),
-            Scalar(0, 0, 255));
-    }
-    imshow("Histogram", histImage);
+    calcHist(&image, 1, channels, Mat(), hist, 1, histSize, ranges);
 }
-*/
 
+//히스토그램 드로잉 함수
+void draw_histo(Mat hist, Mat& hist_img, Size size = Size(256,200)) {
+    hist_img = Mat(size, CV_8U, Scalar(255)); //그래프 행렬
+    float bin = (float)hist_img.cols / hist.rows; // 한 계급 너비
+    normalize(hist, hist, 0, hist_img.rows, NORM_MINMAX);
+
+    for (int i = 0; i < hist.rows; i++)
+    {
+        float start_x = i * bin; // 막대 사각형 시작 X 좌표
+        float end_x = (i + 1) * bin; // 막대 사각형 종료 x 좌표
+        Point2f pt1(start_x, 0);
+        Point2f pt2(end_x, hist.at<float>(i));
+
+        if (pt2.y > 0)
+            rectangle(hist_img, pt1, pt2, Scalar(0), -1); // 막대 사각형 그리기
+    }
+    flip(hist_img, hist_img, 0); // x축 기준 영상 뒤집기
+}
 
 int main(int /*argc*/, char* /*argv*/[])
 {
@@ -61,8 +72,8 @@ int main(int /*argc*/, char* /*argv*/[])
     int histogram[256] = { 0 };
 
     //임계값 설정
-    double threshold_max = 0.03;
-    double threshold_min = 0.05;
+    double threshold_max = 0.05;
+    double threshold_min = 0.02;
 
     //시간 경과를 보여줄 것
     int time_watch = 0;
@@ -75,7 +86,7 @@ int main(int /*argc*/, char* /*argv*/[])
     PylonInitialize();
 
     //템블릿 이미지 저장
-    Mat templ = imread("templ_4_1.png", cv::IMREAD_GRAYSCALE);
+    Mat templ = imread("templ_04_10.png", cv::IMREAD_GRAYSCALE);
     //Mat templ = imread("templ_4_1.png");
     Mat templ2 = imread("templ_5.png");
 
@@ -159,7 +170,7 @@ int main(int /*argc*/, char* /*argv*/[])
                 //작동시간 체크 이후 GRAY로 변경, 그 뒤 작동시간 체크, geometric, masking기법 사용
                 for (int i = 0; i < result.rows; i++) {
                     for (int j = 0; j < result.cols; j++) {
-                        if (result.at<float>(i, j) <= threshold_min && result.at<float>(i, j) >= threshold_max) {
+                        if (result.at<float>(i, j) >= threshold_min && result.at<float>(i, j) <= threshold_max) {
                             // 임계값 이상의 좌표에 template(pattern) 크기만큼을 더해 사각형을 그림.
                             // OpenCV의 경우 RGB 순서가 아닌 BGR 순서로 표시함.
                             rectangle(img_out, Point(j, i), Point(j + templ.cols, i + templ.rows), Scalar(0, 0, 255), 1);
@@ -172,32 +183,16 @@ int main(int /*argc*/, char* /*argv*/[])
                 end = clock();
                 double searching_time = difftime(end, start) / CLOCKS_PER_SEC;
 
-                int hist_w = ptrGrabResult->GetWidth();
-                int hist_h = ptrGrabResult->GetHeight();
-                int bin_w = cvRound((double)hist_w / 256);
-
-                Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(255, 255, 255));
-
-                int max = histogram[0];
-                for (int i = 1; i < 256; i++) {
-                    histogram[i] = floor(((double)histogram[i] / max) * histImage.rows);
-                }
-
-                for (int i = 0; i < 255; i++) {
-                    line(histImage, Point(bin_w * (i), hist_h),
-                        Point(bin_w * (i), hist_h - histogram[i]),
-                        Scalar(0, 0, 255));
-                }
-                imshow("Histogram", histImage);
+                Mat hist, hist_img;
+                calc_Histo(src, hist, 256); //히스토그램 계산함수
+                draw_histo(hist, hist_img); // 히스토그램 그래프 드로잉
 
                 //drawHist(histogram);
 
                 cout << "연산시간 : " << searching_time << endl << endl;
 
-                //cout << "working time : " << endl << endl; // 작동하는 시간 추가
-
-                 // imshow 이미지 출력 함수
-
+                // imshow 이미지 출력 함수
+                imshow("hist_img", hist_img);
                 imshow("src", img_out);
                 imshow("templ", templ);
                 imshow("result", result);
